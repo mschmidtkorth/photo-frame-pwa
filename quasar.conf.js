fe6cadbc9a5b4c541a672ebdcfcffd0296b16ff0
Charlie Knoll <charlie.knoll@gmail.com>
2020-12-17 14:59:16 -0700

    'Deploy'

--- quasar.conf.js
+++ quasar.conf.js
@@ -127,7 +127,7 @@ module.exports = function (/* ctx */) {
         short_name: 'PhotoAh',
         description:
           'Invite your friends and family to add photos to your shared Google Photos album, then use this app to play them on your phone/tablet/desktop',
-        display: 'standalone',
+        display: 'fullscreen',
         start_url: '/',
         background_color: '#ffffff',
         theme_color: '#284D87',
